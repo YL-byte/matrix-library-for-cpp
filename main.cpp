8d9bdc4f054cd22b92aefb5ffefcb6160b2aae5e
YL-byte <56436958+YL-byte@users.noreply.github.com>
2021-10-25 12:19:41 +0300

    All Main Features of the library
    An example file of all the main features of the matrix library.

--- main.cpp
+++ main.cpp
@@ -3,43 +3,46 @@
 #include "matrix.cpp"
 
 int main(){
-    Matrix A(5, 4), B(3, 3), C(4, 4);
-    A.populateWithDiagonalValue_();
-    B.populateWithRisingIntegers_(2);
-    C.populateWithDiagonalValue_(2);
-    A.matrix[0][1] = 1;
-    A.matrix[1][0] = 1;
-    A.matrix[3][2] = 1;
-    A.matrix[0][2] = 0;
-    A.matrix[0][3] = 4;
+    Matrix A(4, 4);
+    float A_arr[16] = {
+        2, 2, 3, 3,
+        2, 3, 1, 0,
+        1, 2, 0 ,0,
+        5, 0 ,0 ,0
+    };
 
-    printf("Matrix C:\n");
-    C.printMatrix();
-    Matrix CR = reducedRowEchelonForm(C);
-    printf("Reduced Row Echelon Form C:\n");
-    CR.printMatrix();
-    printf("Matrix C Det: %f\n", C.det());
+    A = A_arr;
+    float det = A.det();
+    int r = A.rank();
+    Matrix A_INV = A.inverse();
+    Matrix A_T = A.transpose();
+    Matrix I = A * A_INV;
 
-    Matrix BR = reducedRowEchelonForm(B);
-    printf("Matrix B:\n");
-    B.printMatrix();
-    printf("Reduced Row Echelon Form B:\n");
-    BR.printMatrix();
-    printf("Matrix B Det: %f\n", B.det());
-
-    Matrix AR = reducedRowEchelonForm(A);
-    printf("Matrix A:\n");
+    printf("The following example matrix properties:\n\trank: %d\n\tdeterminant: %.2f\n\n", r, det);
     A.printMatrix();
-    printf("Reduced Row Echelon Form A:\n");
-    AR.printMatrix();
-    printf("Matrix A Det: %f", A.det());
+    printf("The Inverse of the matrix is:\n");
+    A_INV.printMatrix();
+    printf("The Transpose of the matrix is:\n");
+    A_T.printMatrix();
+    printf("The dot product of the matrix with it's inverse is:\n");
+    I.printMatrix();
 
     A.freeMatrix_();
-    B.freeMatrix_();
-    C.freeMatrix_();
+    A_INV.freeMatrix_();
+    A_T.freeMatrix_();
+    I.freeMatrix_();
 
-    AR.freeMatrix_();
-    BR.freeMatrix_();
-    CR.freeMatrix_();
+    printf("_______New Example_______\n\n");
+    Matrix B(3, 3);
+    B.populateWithRisingIntegers_();
+    det = B.det();
+    r = B.rank();
+    Matrix B_reduced = B.reducedRowEchelonForm();
+    printf("The following example matrix properties:\n\trank: %d\n\tdeterminant: %.2f\n\n", r, det);
+    B.printMatrix();
+    printf("The reduced row echelon form is:\n");
+    B_reduced.printMatrix();
+    B.freeMatrix_();
+    B_reduced.freeMatrix_();
 
 }

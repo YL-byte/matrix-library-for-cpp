d3f2cd191688c94ac1c71b1789182cbd7a9ba30c
YL-byte <56436958+YL-byte@users.noreply.github.com>
2021-10-25 20:54:10 +0300

    Update example_two_linear_equation_solver.cpp

--- example_two_linear_equation_solver.cpp
+++ example_two_linear_equation_solver.cpp
@@ -9,38 +9,48 @@ int main(){
         2, 2, 3, 3,
         2, 3, 1, 0,
         1, 2, 0 ,0,
-        5, 0 ,0 ,0
+        1, 0 ,1 ,0
     };
 
     float S_arr[5] = {
-        0,
-        0,
-        0,
-        1
+        3,
+        1,
+        2,
+        3
     };
 
     M = M_arr;
     S = S_arr;
-    Matrix U = joinMatrices(M, S);
+
+    Matrix J = joinMatrices(M, S);
     int r_M = M.rank();
-    int r_U = U.rank();
-    if(r_U > r_M){
+    int r_J = J.rank();
+    printf("Try To Solve M*X = S For X\n");
+    if(r_J > r_M){
         printf("There Is No Solution.\n");
     }
-    else if (r_U == r_M && r_U == U.rows){
+    else if (r_J == r_M && r_J == J.rows){
         printf("The Solution To The Following System Of Equations:\n");
-        U.printMatrix();
-        U.reducedRowEchelonForm_();
+        J.printMatrix();
+        J.reducedRowEchelonForm_();
         printf("Is:\n");
-        Matrix X = U.subMatrix(0, 4, 4, 5);
+        Matrix X = J.subMatrix(0, 4, 4, 5);
         X.printMatrix();
         X.freeMatrix_();
+        printf("Another Way To Solve The Matrix Is By Multiplying Both Sides By M^-1 From The Left:\n");
+        Matrix M_inv = M.inverse();
+        M_inv.printMatrix();
+        Matrix X1 = M_inv * S;
+        X1.printMatrix();
+        X1.freeMatrix_();
+
     }
     else {
         printf("There Are Infinite Solutions:\n");
-        U.printMatrix();
+        J.printMatrix();
     }
-    M.freeMatrix_();
+
     S.freeMatrix_();
-    U.freeMatrix_();
+    M.freeMatrix_();
+    J.freeMatrix_();
 }
